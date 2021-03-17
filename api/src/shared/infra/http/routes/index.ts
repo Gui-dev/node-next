3d9b178a66a7929fcaa9ee90ab98a0480cffc0ec
Gui-dev <adrianon10@gmail.com>
2021-03-17 03:09:19 -0300

    feat: add routes to surveys

--- api/src/shared/infra/http/routes/index.ts
+++ api/src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import usersRouters from '@modules/users/infra/http/routes/users.routes'
+import surveyRouters from '@modules/surveys/infra/http/routes/surveys.routes'
 
 const routes = Router()
 
 routes.use('/users', usersRouters)
+routes.use('/surveys', surveyRouters)
 
 export default routes
