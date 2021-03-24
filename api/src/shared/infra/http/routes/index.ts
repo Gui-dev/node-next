f6e124f1bd56ad8416edddb18509b8f74289cecf
Gui-dev <adrianon10@gmail.com>
2021-03-24 20:38:06 -0300

    feat: add route answer.routes

--- api/src/shared/infra/http/routes/index.ts
+++ api/src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import usersRouters from '@modules/users/infra/http/routes/users.routes'
 import surveyRouters from '@modules/surveys/infra/http/routes/surveys.routes'
 import surveysUsersRouters from '@modules/surveysUsers/infra/http/routes/surveysUsers.routes'
+import answerRouters from '@modules/answer/infra/http/routes/answer.routes'
 
 const routes = Router()
 
 routes.use('/users', usersRouters)
 routes.use('/surveys', surveyRouters)
 routes.use('/surveys-users', surveysUsersRouters)
+routes.use('/answers', answerRouters)
 
 export default routes
