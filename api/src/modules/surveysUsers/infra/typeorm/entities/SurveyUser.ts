1a61cd2049572d24b4e0914bc02eeb445e5a93ce
Gui-dev <adrianon10@gmail.com>
2021-03-24 02:55:30 -0300

    feat: add property value and relations between table User and Survey

--- api/src/modules/surveysUsers/infra/typeorm/entities/SurveyUser.ts
+++ api/src/modules/surveysUsers/infra/typeorm/entities/SurveyUser.ts
@@ -1,5 +1,8 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm'
 import { v4 as uuid } from 'uuid'
+
+import { User } from '@modules/users/infra/typeorm/entities/User'
+import { Survey } from '@modules/surveys/infra/typeorm/entities/Survey'
 
 @Entity('surveys_users')
 export class SurveyUser {
@@ -9,8 +12,19 @@ export class SurveyUser {
   @Column()
   public readonly user_id: string
 
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  public readonly user: User
+
   @Column()
   public readonly survey_id: string
+
+  @ManyToOne(() => Survey)
+  @JoinColumn({ name: 'survey_id' })
+  public readonly survey: Survey
+
+  @Column()
+  public readonly value: number
 
   @CreateDateColumn()
   public readonly created_at: Date
