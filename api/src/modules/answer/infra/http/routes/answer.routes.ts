d0c9460cdcd6c2540e7390ce40fd6d87d8298bd5
Gui-dev <adrianon10@gmail.com>
2021-03-25 02:57:09 -0300

    feat: add route to nps

--- api/src/modules/answer/infra/http/routes/answer.routes.ts
+++ api/src/modules/answer/infra/http/routes/answer.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import { AnswerController } from '../controllers/AnswerController'
+import { NpsController } from '../controllers/NpsController'
 
 const answerRouters = Router()
 const answerController = new AnswerController()
+const npsController = new NpsController()
 
 answerRouters.get('/:value', answerController.create)
+answerRouters.get('/nps/:survey_id', npsController.create)
 
 export default answerRouters
