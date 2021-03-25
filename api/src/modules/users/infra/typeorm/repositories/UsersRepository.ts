5cdd0fa2bb46f24ea8f540d1749f9fd5861a0820
Gui-dev <adrianon10@gmail.com>
2021-03-25 03:27:33 -0300

    feat: add helper method userValidation and try/catch

--- api/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ api/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm'
 
 import { AppError } from '@shared/error/AppError'
 import { User } from '@modules/users/infra/typeorm/entities/User'
+import { userValidation } from '../../http/validation/userValidation'
 
 interface IUsersRepository {
   name: string
@@ -12,19 +13,25 @@ export class UsersRepository {
   public async execute ({ name, email }: IUsersRepository): Promise<User> {
     const userRepository = getRepository(User)
 
-    const userAlreadyExists = await userRepository.findOne({ email })
+    try {
+      await userValidation.validate({ name, email })
 
-    if (userAlreadyExists) {
-      throw new AppError('Email address already used')
+      const userAlreadyExists = await userRepository.findOne({ email })
+
+      if (userAlreadyExists) {
+        throw new AppError('Email address already used')
+      }
+
+      const user = userRepository.create({
+        name,
+        email
+      })
+
+      await userRepository.save(user)
+
+      return user
+    } catch (error) {
+      throw new AppError(error)
     }
-
-    const user = userRepository.create({
-      name,
-      email
-    })
-
-    await userRepository.save(user)
-
-    return user
   }
 }
