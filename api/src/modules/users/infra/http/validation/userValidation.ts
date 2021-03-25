8432d08bc23f3babe625229ffd344094bfdc167d
Gui-dev <adrianon10@gmail.com>
2021-03-25 03:26:48 -0300

    feat: add user validation

---
+++ api/src/modules/users/infra/http/validation/userValidation.ts
@@ -0,0 +1,6 @@
+import * as Yup from 'yup'
+
+export const userValidation = Yup.object().shape({
+  name: Yup.string().required('Nome é obrigatório'),
+  email: Yup.string().email('E-mail inválido').required('E-mail é obrigatório')
+})
