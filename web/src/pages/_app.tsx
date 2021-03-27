684ed7d5b4aafb082c2174a0821abcec93c6d2c8
Gui-dev <adrianon10@gmail.com>
2021-03-27 04:22:49 -0300

    feat: add component ExperienceBar

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from 'next/app'
+
 import GlobalStyle from './../styles/global'
+import { ExperienceBar } from './../components/ExperienceBar'
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <ExperienceBar />
       <Component { ...pageProps }/>
     </>
   )
