a44e6c576744659fce72e330bd61270895c896e5
Gui-dev <adrianon10@gmail.com>
2021-03-31 03:16:56 -0300

    style: add styles from page Home

---
+++ web/src/styles/home.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 99.2rem;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 2.5rem 2rem;
+
+  section {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 10rem;
+    flex: 1;
+    align-content: center;
+  }
+`
