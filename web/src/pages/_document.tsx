5e878224a14586856f4c03286096fddba26c829a
Gui-dev <adrianon10@gmail.com>
2021-03-31 02:33:42 -0300

    feat: add title Moveit

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -36,6 +36,7 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+          <title>Moveit</title>
         </Head>
         <body>
           <Main />
