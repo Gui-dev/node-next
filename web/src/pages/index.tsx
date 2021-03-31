10025fe6019fc4747bf33b8082dd08a227a7c7f8
Gui-dev <adrianon10@gmail.com>
2021-03-31 03:19:25 -0300

    feat: add component Profile

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,16 @@
+import { Profile } from '../components/Profile'
+import { Container } from './../styles/home'
+
 export default function Home () {
   return (
-    <></>
+    <Container>
+      <section>
+        <div>
+          <Profile />
+        </div>
+
+        <div></div>
+      </section>
+    </Container>
   )
 }
