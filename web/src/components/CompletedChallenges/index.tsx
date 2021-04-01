bc2fd90f564a668c62db163096cf0bdc7715bd58
Gui-dev <adrianon10@gmail.com>
2021-04-01 02:47:57 -0300

    feat: add component CompletedChallenges and styles

---
+++ web/src/components/CompletedChallenges/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './style'
+
+export const CompletedChallenges: React.FC = () => {
+  return (
+    <Container>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </Container>
+  )
+}
---
+++ web/src/components/CompletedChallenges/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: 500;
+  margin: 3rem 0;
+  padding: 1rem;
+  border-bottom: 1px solid #D7D8DA;
+
+  span:first-child {
+    font-size: 1.25rem;
+  }
+
+  span:last-child {
+    font-size: 1.5rem;
+  }
+`
