a8c68fa517f2c54ed37830738575f5d3f4ba6933
Gui-dev <adrianon10@gmail.com>
2021-04-05 02:56:51 -0300

    style: remove property height from Container

--- web/src/styles/home.ts
+++ web/src/styles/home.ts
@@ -4,8 +4,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 99.2rem;
-  height: 100vh;
-  margin: 0 auto;
+  /* height: 100vh; */
+  margin: 4rem auto;
   padding: 2.5rem 2rem;
 
   section {
