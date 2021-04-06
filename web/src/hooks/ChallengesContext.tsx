aac43c8aed6bfaec89007c8c860bc920d6645f4e
Gui-dev <adrianon10@gmail.com>
2021-04-06 03:47:20 -0300

    feat: add context Challenges

---
+++ web/src/hooks/ChallengesContext.tsx
@@ -0,0 +1,69 @@
+import { createContext, ReactNode, useContext, useState } from 'react'
+import challenges from './../../challenges.json'
+
+interface IChallengeProps {
+  type: string
+  description: string
+  amount: number
+}
+
+interface IChallengesContextProps {
+  level: number
+  currentExperience: number
+  challengesCompleted: number
+  activeChallenge: IChallengeProps | null
+  experienceToNextLevel: number
+  levelUp: () => void
+  startNewChallenge: () => void
+  resetChallenge: () => void
+}
+
+interface IChallengesProviderProps {
+  children: ReactNode
+}
+
+export const ChallengesContext = createContext({} as IChallengesContextProps)
+
+export const ChallengesProvider = ({ children }: IChallengesProviderProps) => {
+  const [level, setLevel] = useState(1)
+  const [currentExperience, setCurrentExperience] = useState(0)
+  const [challengesCompleted, setChallengesCompleted] = useState(0)
+  const [activeChallenge, setActiveChallenge] = useState<IChallengeProps | null>(null)
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
+  const levelUp = () => {
+    setLevel(level + 1)
+  }
+
+  const startNewChallenge = () => {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+    const challenge = challenges[randomChallengeIndex]
+    setActiveChallenge(challenge)
+  }
+
+  const resetChallenge = () => {
+    setActiveChallenge(null)
+  }
+
+  return (
+    <ChallengesContext.Provider value={{
+      level,
+      currentExperience,
+      challengesCompleted,
+      activeChallenge,
+      experienceToNextLevel,
+      levelUp,
+      startNewChallenge,
+      resetChallenge
+    }}
+    >
+      { children }
+    </ChallengesContext.Provider>
+  )
+}
+
+export const useChallenges = () => {
+  const context = useContext(ChallengesContext)
+  return context
+}
