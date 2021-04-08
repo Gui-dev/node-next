a1ba4252ec0cefde54e0da084c31435aac72b55c
Gui-dev <adrianon10@gmail.com>
2021-04-08 16:07:38 -0300

    refactor: delete tag Hooks

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,18 +2,15 @@ import Head from 'next/head'
 import { AppProps } from 'next/app'
 
 import GlobalStyle from './../styles/global'
-import { ExperienceBar } from './../components/ExperienceBar'
-import { Hooks } from './../hooks'
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
-    <Hooks>
+    <>
       <Head>
         <title>Moveit</title>
       </Head>
       <GlobalStyle />
-      <ExperienceBar />
       <Component { ...pageProps }/>
-    </Hooks>
+    </>
   )
 }
